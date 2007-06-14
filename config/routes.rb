e3f5b62cbccf708ad62d481df4afe13b9ebca350
seth.fitzsimmons <seth.fitzsimmons@77829da8-b331-0410-ae36-b9324180a337>
2007-06-14 18:46:48 +0000

    added logged exceptions

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ ActionController::Routing::Routes.draw do |map|
   # instead of a file named 'wsdl'
   map.connect ':controller/service.wsdl', :action => 'wsdl'
 
+  map.resources :logged_exceptions
+
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
