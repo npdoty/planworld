d9563eb3d6a689aa94cd6d9f36e6af7837cc6430
seth.fitzsimmons <seth.fitzsimmons@77829da8-b331-0410-ae36-b9324180a337>
2007-06-15 04:05:19 +0000

    a modified version of technoweenie's labeled_form_helper

--- vendor/plugins/mojo/init.rb
+++ vendor/plugins/mojo/init.rb
@@ -1,2 +1,6 @@
 require 'mojodna/attachment_migration'
 require 'mojodna/required_attributes'
+require 'mojodna/labeled_form_helper'
+ActionView::Base.send                 :include, MojoDNA::LabeledFormHelper
+ActionView::Helpers::InstanceTag.send :include, MojoDNA::LabeledInstanceTag
+ActionView::Helpers::FormBuilder.send :include, MojoDNA::FormBuilderMethods
